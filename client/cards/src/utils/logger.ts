9f4421a6f07295ad539dc491ebcb100865b7d93b
Muhammad Qasim <qasim9872@gmail.com>
2021-04-18 08:48:18 +0500

    updated logger to also toast

--- client/cards/src/utils/logger.ts
+++ client/cards/src/utils/logger.ts
@@ -1,1 +1,24 @@
-export const logger = console;
+import { toast } from "react-toastify";
+
+class MyLogger {
+  debug(message: string, data?: any) {
+    console.log(message, data);
+  }
+
+  info(message: string, data?: any) {
+    console.info(message, data);
+    toast(message, { type: "info" });
+  }
+
+  warn(message: string, data?: any) {
+    console.warn(message, data);
+    toast(message, { type: "warning" });
+  }
+
+  error(message: string, data?: any) {
+    console.error(message, data);
+    toast(message, { type: "error" });
+  }
+}
+
+export const logger = new MyLogger();
